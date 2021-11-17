3c27221acd8b78c421b167e83a7dee3c25ea5f05
joecaro <jsc11598@gmail.com>
2021-11-17 15:50:12 -0500

    basic home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,46 @@
 import React from "react";
 
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <>
+      <h1>Home Page</h1>
+      <hr />
+      <h2>Hash</h2>
+      <p>
+        Occaecat velit nisi aliquip mollit aliquip velit aliqua amet. Lorem
+        dolor amet anim do adipisicing laborum do ipsum nostrud consectetur quis
+        sit do enim. Pariatur ea occaecat aliquip laboris cupidatat. Ex nulla
+        quis do consectetur est commodo deserunt laboris commodo aliquip quis.
+        Deserunt ea Lorem esse deserunt nulla fugiat ad tempor irure elit
+        labore.
+      </p>
+      <h2>Block</h2>
+      <p>
+        Occaecat velit nisi aliquip mollit aliquip velit aliqua amet. Lorem
+        dolor amet anim do adipisicing laborum do ipsum nostrud consectetur quis
+        sit do enim. Pariatur ea occaecat aliquip laboris cupidatat. Ex nulla
+        quis do consectetur est commodo deserunt laboris commodo aliquip quis.
+        Deserunt ea Lorem esse deserunt nulla fugiat ad tempor irure elit
+        labore.
+      </p>
+      <h2>Blockchain</h2>
+      <p>
+        Occaecat velit nisi aliquip mollit aliquip velit aliqua amet. Lorem
+        dolor amet anim do adipisicing laborum do ipsum nostrud consectetur quis
+        sit do enim. Pariatur ea occaecat aliquip laboris cupidatat. Ex nulla
+        quis do consectetur est commodo deserunt laboris commodo aliquip quis.
+        Deserunt ea Lorem esse deserunt nulla fugiat ad tempor irure elit
+        labore.
+      </p>
+      <h2>Distributed Blockchain</h2>
+      <p>
+        Occaecat velit nisi aliquip mollit aliquip velit aliqua amet. Lorem
+        dolor amet anim do adipisicing laborum do ipsum nostrud consectetur quis
+        sit do enim. Pariatur ea occaecat aliquip laboris cupidatat. Ex nulla
+        quis do consectetur est commodo deserunt laboris commodo aliquip quis.
+        Deserunt ea Lorem esse deserunt nulla fugiat ad tempor irure elit
+        labore.
+      </p>
+    </>
+  );
 }
